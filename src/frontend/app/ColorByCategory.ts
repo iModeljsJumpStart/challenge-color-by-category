46b662475827c3f25ddfcfafddcc11ad36b661bb
Josh Schifter <josh.schifter@bentley.com>
2020-07-07 17:53:33 -0400

    Appease the linter

--- src/frontend/app/ColorByCategory.ts
+++ src/frontend/app/ColorByCategory.ts
@@ -2,14 +2,13 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { ColorDef } from "@bentley/imodeljs-common";
 import { IModelConnection, SelectionSetEvent, Viewport } from "@bentley/imodeljs-frontend";
 import { Id64String } from "@bentley/bentleyjs-core";
 
 // Implement the methods of this class to complete the challenge.
 export class ColorByCategoryListener {
-  private _colorIndex = 0;
-  private static _colors = [ColorDef.fromString("red"), ColorDef.fromString("green"), ColorDef.fromString("blue"), ColorDef.fromString("orange"), ColorDef.fromString("cyan"), ColorDef.fromString("yellow")];
+  // private _colorIndex = 0;
+  // private static _colors = [ColorDef.fromString("red"), ColorDef.fromString("green"), ColorDef.fromString("blue"), ColorDef.fromString("orange"), ColorDef.fromString("cyan"), ColorDef.fromString("yellow")];
   private static _cleanupListener: () => void;
 
   // Construct a new object and register it to be called when an element is selected
@@ -29,14 +28,17 @@ export class ColorByCategoryListener {
   }
 
   // Cycle through the pre-defined colors
+  /*
   private getNextColor() {
+    // To use this method, you must first uncomment _colors and _colorIndex above
     const current = ColorByCategoryListener._colors[this._colorIndex];
     this._colorIndex = (this._colorIndex + 1) % ColorByCategoryListener._colors.length;
     return current;
   }
+  */
 
   // Override the color of the input elements to this.getNextColor()
-  private overrideElementColors(ids: Id64String[]) {
+  private overrideElementColors(_ids: Id64String[]) {
 
     // Challenge: You need to fill in this method to complete the challenge.
     //            This method should call this.getNextColor()
@@ -61,15 +63,17 @@ export class ColorByCategoryListener {
   }
 
   // Query the iModel for the name of the category for the input element.
+  /*
   private async queryForCategoryName(inputId: string, iModel: IModelConnection): Promise<Id64String[]> {
 
     // Bonus: You need to fill in this method to complete the bonus challenge.
     return [];
 
   }
+  */
 
   // Show a message to the user containing the name of the category for the input element.
-  private async showMessageForCategoryOfSelectedElement(inputId: string, iModel: IModelConnection) {
+  private async showMessageForCategoryOfSelectedElement(_inputId: string, _iModel: IModelConnection) {
 
     // Bonus: You need to fill in this method to complete the bonus challenge.
     //        This method should call this.queryForCategoryName(inputId, iModel)
@@ -80,12 +84,12 @@ export class ColorByCategoryListener {
   private _elementSelected = async (ev: SelectionSetEvent) => {
     if (ev.set.elements.size === 1) {
       const sourceElementId = Array.from(ev.set.elements).pop();
-      console.log("ID of selected element: " + sourceElementId);
+      // console.log("ID of selected element: " + sourceElementId);
 
       if (sourceElementId) {
-        this.overrideColorForCategoryOfSelectedElement(sourceElementId, ev.set.iModel);
+        await this.overrideColorForCategoryOfSelectedElement(sourceElementId, ev.set.iModel);
 
-        this.showMessageForCategoryOfSelectedElement(sourceElementId, ev.set.iModel);
+        await this.showMessageForCategoryOfSelectedElement(sourceElementId, ev.set.iModel);
       }
     }
   }
